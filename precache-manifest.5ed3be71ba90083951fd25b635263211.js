44d4e3fe7ba159f4022f0d1841d0accd224a8054
jonathan.forero <jonathan-forero@globant.com>
2019-12-11 01:12:52 -0500

    Updates

---
+++ precache-manifest.5ed3be71ba90083951fd25b635263211.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f837c4329a37debfaeabd03b4b2cbf",
+    "url": "/react-top-number-game/index.html"
+  },
+  {
+    "revision": "37675602f8959b39debc",
+    "url": "/react-top-number-game/static/css/main.e660ad5b.chunk.css"
+  },
+  {
+    "revision": "3681c67f893523d03c45",
+    "url": "/react-top-number-game/static/js/2.a81da23b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-top-number-game/static/js/2.a81da23b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "37675602f8959b39debc",
+    "url": "/react-top-number-game/static/js/main.08fb8862.chunk.js"
+  },
+  {
+    "revision": "b8e0693e31fcefa4c774",
+    "url": "/react-top-number-game/static/js/runtime-main.385830b3.js"
+  }
+]);
